66667d413fd8c17031a0906a69f5ccb74239c5b7
LIMJUWAN <jw.lim@buildit.kr>
2018-04-03 17:05:17 +0900

    test

--- resources/javascript/usb.js
+++ resources/javascript/usb.js
@@ -23,19 +23,9 @@
     });
 
     $('.control-transfer-out').on('click', function (e) {
-        let command = $('.command').val();
-        let requestType = $('.request-type').val();
-        let recipient = $('.recipient').val();
-        let request = $('.request').val();
-        let value = $('.value').val();
-        let index = $('.index').val();
-        let setup = {
-            requestType: requestType,
-            recipient: recipient,
-            request: request,
-            value: value,
-            index: index
-        };
+        let self = $(e.target);
+        let command = self.closest('.command').val();
+        let setup = getControlTransferSetup(self);
 
         device.controlTransferOut(setup, new TextEncoder('utf-8').encode(command))
         .then(data => {
@@ -45,5 +35,37 @@
             console.log(error);
         });
     });
+
+    $('.control-transfer-in').on('click', function (e) {
+        let self = $(e.target);
+        let command = self.closest('.command').val();
+        let setup = getControlTransferSetup(self);
+
+        device.controlTransferIn(setup, command)
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    });
+
+    function getControlTransferSetup(self) {
+        let li = self.closest('li');
+        let requestType = li.find('.request-type').val();
+        let recipient = li.find('.recipient').val();
+        let request = li.find('.request').val();
+        let value = li.find('.value').val();
+        let index = li.find('.index').val();
+        let setup = {
+            requestType: requestType,
+            recipient: recipient,
+            request: request,
+            value: value,
+            index: index
+        };
+
+        return setup;
+    }
 
 })();
